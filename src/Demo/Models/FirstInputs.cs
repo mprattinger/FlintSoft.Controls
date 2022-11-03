65c2461c9569dab2789126c2f4a7eb43a2e37370
Michael Prattinger <mprattinger@outlook.com>
2022-11-03 10:17:08 +0100

    Add project files.

---
+++ src/Demo/Models/FirstInputs.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demo.Models;
+
+public class FirstInputs
+{
+    [Required]
+    public string FirstTextInput { get; set; } = "";
+
+    [Required]
+    public DateOnly FirstDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+}
